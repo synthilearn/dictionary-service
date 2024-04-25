d9b69f795ec0e97f523a66f7a6161a5bdcb17b22
Nikitos2231 <nik.porshennikov@mail.ru>
2024-04-26 00:20:20 +0700

    Добил группировки 3

--- src/main/java/com/synthilearn/dictionaryservice/app/services/util/GroupPaginatorService.java
+++ src/main/java/com/synthilearn/dictionaryservice/app/services/util/GroupPaginatorService.java
@@ -27,8 +27,8 @@ public class GroupPaginatorService {
         }
         TreeMap<String, TreeMap<String, TreeSet<Phrase>>> stringTreeMapTreeMap =
                 switch (requestDto.getGroups().getFirst()) {
-                    case LETTER -> groupPhrasesByFirstLetter(phrases);
-                    case PART_OF_SPEECH -> groupPhrasesByPartOfSpeech(phrases);
+                    case PART_OF_SPEECH -> groupPhrasesByFirstLetter(phrases);
+                    case LETTER -> groupPhrasesByPartOfSpeech(phrases);
                 };
         if (Boolean.FALSE.equals(requestDto.getShowTranslation())) {
             stringTreeMapTreeMap.values().stream()
